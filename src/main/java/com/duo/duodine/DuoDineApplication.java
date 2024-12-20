37c13eae98c7d893697acd64ac8fadccee37d722
이상엽 <infightersy@naver.com>
2024-12-20 16:02:23 +0900

    first commit

---
+++ src/main/java/com/duo/duodine/DuoDineApplication.java
@@ -0,0 +1,13 @@
+package com.duo.duodine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuoDineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuoDineApplication.class, args);
+    }
+
+}
